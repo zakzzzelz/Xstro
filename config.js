592e6549593ef9b9eb1f15e9aff20597ff8f403a
Astro <danielfx016@gmail.com>
2024-11-02 15:53:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const toBool = (x) => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
-export const BOT_INFO = process.env.BOT_INFO || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ;https://img.freepik.com/free-photo/cityscape-anime-inspired-urban-area_23-2151028605.jpg?t=st=1730553887~exp=1730557487~hmac=5ba4fb5c7312cdc27e4605bd9854aa3851f8f56ee4573484b1e65e110ae897c2&w=1480";
+export const BOT_INFO = process.env.BOT_INFO || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ";
 export const STICKER_PACK = process.env.STICKER_PACK || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ";
 export const LOGS = toBool(process.env.LOGS) || true;
 export const PREFIX = process.env.PREFIX || ".";
