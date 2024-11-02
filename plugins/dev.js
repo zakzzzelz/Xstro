ce608500e8830924dad783dbad4e4881e4e1cacf
Astro <danielfx016@gmail.com>
2024-11-02 12:55:52 +0100

    Update dev.js

--- plugins/dev.js
+++ plugins/dev.js
@@ -1,23 +1,24 @@
 import { command } from "../lib/plugins.js";
 import { inspect } from "util";
 
-command({ on: "text" }, async (message, match, m, client) => {
+command({ on: "text", dontAddCommandList: true }, async (message, match, m, client) => {
 	if (!match.startsWith("$ ")) return;
-	const code = match.slice(2);
+	const code = match.slice(2).trim();
 	const executeCode = async (code) => {
-		const context = { message, match, m, client };
+		const context = { message, match, m, client, ...process.env };
 		const keys = Object.keys(context);
 		const values = Object.values(context);
-		const func = new Function(...keys, `return (async () => { ${code} })();`);
-		return await func(...values);
+		const func = `async () => { return (${code}); }`;
+		return await eval(func).apply(null, values);
 	};
+
 	try {
 		let result = await executeCode(code);
 		result = typeof result === "function" ? result.toString() : inspect(result, { depth: 1 });
 		const formattedOutput = `*Result:*\n\`\`\`${result}\`\`\``;
-		await message.send(formattedOutput);
+		await message.sendReply(formattedOutput);
 	} catch (error) {
 		const errorOutput = `_Error:_ ${error.message}`;
-		await message.send(errorOutput);
+		await message.sendReply(errorOutput);
 	}
 });
