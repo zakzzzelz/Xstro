f0588248bfe4130f88abd9b4159e64402b995dd5
Astro <danielfx016@gmail.com>
2024-11-02 15:57:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 const toBool = (x) => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
-export const BOT_INFO = process.env.BOT_INFO || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ";
-export const STICKER_PACK = process.env.STICKER_PACK || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ";
+export const BOT_INFO = process.env.BOT_INFO || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ;https://raw.githubusercontent.com/ASTRO-X10/xstro-md/refs/heads/master/media/lite.jpg";
 export const LOGS = toBool(process.env.LOGS) || true;
 export const PREFIX = process.env.PREFIX || ".";
 export const SUDO = process.env.SUDO || "";
+export const STICKER_PACK = process.env.STICKER_PACK || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ";
 export const AUTO_READ = toBool(process.env.AUTO_READ) || false;
 export const AUTO_STATUS_READ = toBool(process.env.AUTO_STATUS_READ) || false;
 export const WORK_TYPE = process.env.WORK_TYPE || "private";
