58c443a9f73a74990453ce16534cd91e35b48068
Daniel <danielfx016@gmail.com>
2024-11-03 08:15:46 +0000

    fix

--- plugins/sudo.js
+++ plugins/sudo.js
@@ -32,15 +32,20 @@ bot(
     }
 )
 
-bot({ pattern: 'getsudo', alias: 'listsudo', type: 'user' }, async m => {
-    let s = await getSudo()
-    if (s === '_No Sudo Numbers_') return m.sendReply('*_Sudo Users_*\n\n_No Sudo Numbers_')
-    let list = '*_Sudo Users_*\n\n' + s.split('\n').map((u, i) =>
-        `${i + 1}. @${u.replace(/User[s]?:\s*/, '').replace('@s.whatsapp.net', '').trim()}`
-    ).join('\n')
-    return m.sendReply(list, {
-        mentions: s.split('\n').map(u =>
-            `${u.replace('@s.whatsapp.net', '').trim()}@s.whatsapp.net`
-        )
+bot(
+    {
+        pattern: 'getsudo',
+        alias: 'listsudo',
+        type: 'user'
+    }, async m => {
+        let s = await getSudo()
+        if (s === '_No Sudo Numbers_') return m.sendReply('*_Sudo Users_*\n\n_No Sudo Numbers_')
+        let list = '*_Sudo Users_*\n\n' + s.split('\n').map((u, i) =>
+            `${i + 1}. @${u.replace('@s.whatsapp.net', '').trim()}`
+        ).join('\n')
+        return m.sendReply(list, {
+            mentions: s.split('\n').map(u =>
+                `${u.replace('@s.whatsapp.net', '').trim()}@s.whatsapp.net`
+            )
+        })
     })
-})
