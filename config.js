b7072360a19dbf2f6b0056e4055006d256422624
Daniel <danielfx016@gmail.com>
2024-11-03 15:02:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,33 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-import { createRequire } from "module";
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 dotenv.config();
 
-const toBool = (x) => x === "true";
-const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
-export const BOT_INFO = process.env.BOT_INFO || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ;https://raw.githubusercontent.com/ASTRO-X10/xstro-md/refs/heads/master/media/lite.jpg";
+const toBool = x => x === 'true';
+const DATABASE_URL = process.env.DATABASE_URL || './database.db';
+export const BOT_INFO = process.env.BOT_INFO || 'ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ;https://raw.githubusercontent.com/ASTRO-X10/xstro-md/refs/heads/master/media/lite.jpg';
 export const LOGS = toBool(process.env.LOGS) || true;
-export const PREFIX = process.env.PREFIX || ".";
-export const SUDO = process.env.SUDO || "";
-export const STICKER_PACK = process.env.STICKER_PACK || "ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ";
+export const PREFIX = process.env.PREFIX || '.';
+export const SUDO = process.env.SUDO || '';
+export const STICKER_PACK = process.env.STICKER_PACK || 'ᴀsᴛʀᴏ;xsᴛʀᴏ-ᴍᴅ';
 export const AUTO_READ = toBool(process.env.AUTO_READ) || false;
 export const AUTO_STATUS_READ = toBool(process.env.AUTO_STATUS_READ) || false;
-export const MODE = process.env.MODE || "private";
-export const GITHUB_URL = process.env.GITHUB_URL || "https://github.com/ASTRO-X10/xstro-md";
-export const VERSION = require('./package.json').version
+export const MODE = process.env.MODE || 'private';
+export const GITHUB_URL = process.env.GITHUB_URL || 'https://github.com/ASTRO-X10/xstro-md';
+export const VERSION = require('./package.json').version;
 export const DATABASE =
-	DATABASE_URL === "./database.db"
+	DATABASE_URL === './database.db'
 		? new Sequelize({
-				dialect: "sqlite",
+				dialect: 'sqlite',
 				storage: DATABASE_URL,
 				logging: false,
 		  })
 		: new Sequelize(DATABASE_URL, {
-				dialect: "postgres",
+				dialect: 'postgres',
 				ssl: true,
-				protocol: "postgres",
+				protocol: 'postgres',
 				dialectOptions: {
 					native: true,
 					ssl: { require: true, rejectUnauthorized: false },
