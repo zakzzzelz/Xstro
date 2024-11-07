5cbae857d5a6ce1508f722ceae8334c1551d6c09
Daniel <danielfx016@gmail.com>
2024-11-07 17:57:57 +0100

    Create plugin.js

---
+++ plugins/plugin.js
@@ -0,0 +1,12 @@
+import { bot } from '../lib/client/plugins.js
+
+bot(
+  {
+    pattern: 'install ?(.*)',
+    desc: 'Installs A Plugin',
+    type: 'system'
+  },
+  async (message, match) => {
+    /** functionality here **/
+  }
+  )
