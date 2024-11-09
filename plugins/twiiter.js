36a647769ddb442537991bf788cbb87c27194a2d
Daniel <danielfx016@gmail.com>
2024-11-09 19:26:55 +0100

    x

---
+++ plugins/twiiter.js
@@ -0,0 +1,19 @@
+import { bot } from '../lib/client/plugins.js';
+import { twitter } from '../lib/extras/twitter.js';
+import { extractUrlFromMessage } from '../lib/utils.js';
+
+bot(
+	{
+		pattern: 'twitter ?(.*)',
+		desc: 'downloads x videos',
+		type: 'download',
+	},
+	async (message, match) => {
+		const id = match || message.quoted?.text;
+		const url = extractUrlFromMessage(id);
+		if (!url) return message.sendReply('_Provide X Url_');
+		const media = await twitter(url);
+		const { buffer, caption } = media;
+		return await message.send(buffer, { caption: caption });
+	},
+);
