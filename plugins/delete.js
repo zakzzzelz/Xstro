ef011595c09e409e5ca5e6afbe76186c41cfe749
Daniel <danielfx016@gmail.com>
2024-11-09 08:59:41 +0000

    fix

---
+++ plugins/delete.js
@@ -0,0 +1,14 @@
+import { bot } from '../lib/client/plugins.js';
+
+bot(
+	{
+		pattern: 'dlt',
+		desc: 'Deletes Message',
+		type: 'whatsapp',
+	},
+	async message => {
+		if (!message.quoted) return message.sendReply('_Reply A Message_');
+		const msg = message?.quoted;
+		return await message.delete(msg);
+	},
+);
