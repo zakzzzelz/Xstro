84a8de56d0844913b7fb8249b843fa8c92ad8586
Daniel <danielfx016@gmail.com>
2024-11-09 19:47:57 +0100

    Update sudo.js

--- plugins/sudo.js
+++ plugins/sudo.js
@@ -41,7 +41,7 @@ bot(
 		const sudoList = await getSudo();
 		if (sudoList === '_No Sudo Numbers_') return message.sendReply('*_No Sudo Users_*');
 		const sudoNumbers = sudoList.split('\n').map(number => number.replace('@', '').replace('@s.whatsapp.net', '').trim());
-		const formattedSudoList = '*Sudo Users*\n\n' + sudoNumbers.map((number, index) => `${index + 1}. ${number}`).join('\n');
+		const formattedSudoList = '*Sudo Users*\n\n' + sudoNumbers.map((number, index) => `${index + 1}. @${number}`).join('\n');
 		const mentions = sudoNumbers.map(number => `${number}@s.whatsapp.net`);
 		return message.sendReply(formattedSudoList, { mentions });
 	},
