45755febf10fc1f92fcac5cf19d3f9989a152fec
Daniel <danielfx016@gmail.com>
2024-11-10 18:31:01 +0000

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,33 +1,21 @@
 module.exports = {
 	apps: [
-		{
-			name: 'xstro-md',
-			script: './server.js',
-			log_date_format: false,
-			timestamp: false,
-			time: false,
-			instances: 1,
-			exec_mode: 'fork',
-			watch: false,
-			env: {
-				NODE_ENV: 'development',
-				PORT: 8000,
-				PM2_TIME: false,
-				PM2_PREFIX_MSG: false,
-				PM2_LOG_DATE_FORMAT: false,
+			{
+					name: 'xstro-md',
+					script: './index.js',
+					instances: 1,
+					exec_mode: 'cluster',
+					env: {
+							NODE_ENV: 'development',
+							PORT: 8000,
+					},
+					env_production: {
+							NODE_ENV: 'production',
+							PORT: 8000,
+					},
+					output: './out.log',
+					error: './error.log',
+					merge_logs: true,
 			},
-			env_production: {
-				NODE_ENV: 'production',
-				PORT: 8000,
-				PM2_TIME: false,
-				PM2_PREFIX_MSG: false,
-				PM2_LOG_DATE_FORMAT: false,
-			},
-			output: '/dev/stdout',
-			error: '/dev/stderr',
-			log_type: 'raw',
-			merge_logs: true,
-			log: false,
-		},
 	],
 };
