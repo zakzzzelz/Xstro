4ce0c10718598b3129672d2fdb6ebe24fcd43271
Daniel <danielfx016@gmail.com>
2024-11-10 11:10:26 +0100

    Create pinchat.js

---
+++ plugins/pinchat.js
@@ -0,0 +1,27 @@
+import { bot } from '../lib/client/plugins.js';
+
+bot(
+	{
+		pattern: 'pin',
+		isPublic: false,
+		desc: 'pin a chat',
+		type: 'whatsapp',
+	},
+	async (message, match, m, client) => {
+		await client.chatModify({ pin: true }, message.jid);
+		return message.reply('_Pined.._');
+	},
+);
+
+bot(
+	{
+		pattern: 'unpin ?(.*)',
+		isPublic: false,
+		desc: 'unpin a msg',
+		type: 'whatsapp',
+	},
+	async (message, match, m, client) => {
+		await client.chatModify({ pin: false }, message.jid);
+		return message.reply('_Unpined.._');
+	},
+);
