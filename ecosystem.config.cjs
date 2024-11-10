5ef2cd626840c3235a67cec9342f5c606e2be105
Daniel <danielfx016@gmail.com>
2024-11-10 18:32:24 +0000

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,21 +1,18 @@
 module.exports = {
 	apps: [
-			{
-					name: 'xstro-md',
-					script: './index.js',
-					instances: 1,
-					exec_mode: 'cluster',
-					env: {
-							NODE_ENV: 'development',
-							PORT: 8000,
-					},
-					env_production: {
-							NODE_ENV: 'production',
-							PORT: 8000,
-					},
-					output: './out.log',
-					error: './error.log',
-					merge_logs: true,
+		{
+			name: 'xstro-md',
+			script: './index.js',
+			instances: 1,
+			exec_mode: 'cluster',
+			env: {
+				NODE_ENV: 'development',
+				PORT: 8000,
 			},
+			env_production: {
+				NODE_ENV: 'production',
+				PORT: 8000,
+			},
+		},
 	],
 };
