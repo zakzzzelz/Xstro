c0cdf56fd5a7956a016624d2eb110b96fae451d3
Daniel <danielfx016@gmail.com>
2024-11-11 17:23:43 +0100

    Update gdesc.js

--- plugins/gdesc.js
+++ plugins/gdesc.js
@@ -11,7 +11,7 @@ bot(
 		if (!message.isGroup) return message.sendReply('_For groups only!_');
 		if (!m.isAdmin && !m.isBotAdmin) return message.sendReply('_For Admins Only!_');
 		const desciption = match || message.quoted?.text;
-		await client.groupUpdateDescription(message.jid, { desc: desciption });
+		await client.groupUpdateDescription(message.jid, desciption);
 		return message.sendReply('_Group Description Updated_');
 	},
 );
