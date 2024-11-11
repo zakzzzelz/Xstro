8713ab59c5bae06ccf3401f4db5d20c2b5e190ac
Daniel <danielfx016@gmail.com>
2024-11-11 21:19:53 +0100

    Update tag.js

--- plugins/tag.js
+++ plugins/tag.js
@@ -22,21 +22,22 @@ bot(
 );
 
 bot(
-	{
-		pattern: 'tagall',
-		isPublic: true,
-		desc: 'Tag all participants in the group',
-		type: 'group',
-	},
-	async (message, match, m, client) => {
-		if (!m.isGroup) return message.sendReply('_For groups only!_');
-		const msg = match || message.quoted?.text;
-		if (!msg) return message.sendReply('_You Must Provide A Reason for tagging everyone_');
-		const participants = await client.groupMetadata(message.jid);
-		const participantJids = participants.participants.map(p => p.id);
-		await client.sendMessage(message.jid, {
-			text: msg,
-			mentions: participantJids,
-		});
-	},
+  {
+    pattern: 'tagall',
+    isPublic: true,
+    desc: 'Tag all participants in the group',
+    type: 'group',
+  },
+  async (message, match, m, client) => {
+    if (!m.isGroup) return message.sendReply('_For groups only!_');
+    const msg = match || message.quoted?.text;
+    if (!msg) return message.sendReply('_You must provide a reason for tagging everyone._');
+    const participants = await client.groupMetadata(message.jid);
+    const participantJids = participants.participants.map(p => p.id);
+    const tagMsg = `${msg}\n\n` + participantJids.map(jid => `@${jid.split('@')[0]}`).join('\n');
+    await client.sendMessage(message.jid, {
+      text: tagMsg,
+      mentions: participantJids,
+    });
+  },
 );
