9681106cf34fec813100780a2dc4d9a134476742
Daniel <danielfx016@gmail.com>
2024-11-11 20:17:48 +0100

    Update tag.js

--- plugins/tag.js
+++ plugins/tag.js
@@ -9,17 +9,33 @@ bot(
 	},
 	async (message, match, m, client) => {
 		if (!m.isGroup) return message.sendReply('_For groups only!_');
-		if (!m.isAdmin && !m.isBotAdmin) return message.sendReply('_For Admins Only!_');
 		const msg = match || message.quoted?.text;
 		const text = msg || '';
-
 		const participants = await client.groupMetadata(message.jid);
 		const participantJids = participants.participants.map(p => p.id);
-
 		let taggedMessage = text ? `*${text}*` : '';
-
 		await client.sendMessage(message.jid, {
 			text: taggedMessage,
+			mentions: participantJids,
+		});
+	},
+);
+
+bot(
+	{
+		pattern: 'tagall',
+		isPublic: true,
+		desc: 'Tag all participants in the group',
+		type: 'group',
+	},
+	async (message, match, m, client) => {
+		if (!m.isGroup) return message.sendReply('_For groups only!_');
+		const participants = await client.groupMetadata(message.jid);
+		const participantJids = participants.participants.map(p => p.id);
+		const msg = match || message.quoted?.text;
+		if (!msg) return message.sendReply('_You Must Provide A Reason for tagging everyone_');
+		await client.sendMessage(message.jid, {
+			text: msg,
 			mentions: participantJids,
 		});
 	},
