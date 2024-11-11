a7008f4a0fdcc219d972fc29989e9d430ec826aa
Daniel <danielfx016@gmail.com>
2024-11-11 17:38:22 +0100

    Create revoke.js

---
+++ plugins/revoke.js
@@ -0,0 +1,16 @@
+import { bot } from '../lib/client/plugins.js';
+
+bot(
+	{
+		pattern: 'revoke',
+		isPublic: false,
+		desc: 'Revoke Invite link',
+		type: 'group',
+	},
+	async (message, match, m, client) => {
+		if (!m.isGroup) return message.sendReply('_For groups only!_');
+		if (!m.isAdmin && !m.isBotAdmin) return message.sendReply('_For Admins Only!_');
+		await client.groupRevokeInvite(message.jid);
+		return message.sendReply('_Group Link Revoked!_');
+	},
+);
