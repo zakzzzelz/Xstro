5050c3fa6b9a742f60396b38ce259c63f9eb300b
Daniel <danielfx016@gmail.com>
2024-11-11 17:22:16 +0100

    Update gdesc.js

--- plugins/gdesc.js
+++ plugins/gdesc.js
@@ -10,8 +10,8 @@ bot(
 	async (message, match, m, client) => {
 		if (!message.isGroup) return message.sendReply('_For groups only!_');
 		if (!m.isAdmin && !m.isBotAdmin) return message.sendReply('_For Admins Only!_');
-		if (!match) return message.sendReply('_Provide Description to Update Group Desc With!_');
-		await client.groupUpdateDescription(message.jid, { desc: match });
+		const desciption = match || message.quoted?.text;
+		await client.groupUpdateDescription(message.jid, { desc: desciption });
 		return message.sendReply('_Group Description Updated_');
 	},
 );
