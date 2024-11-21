4508d40a2701348cdf3aaf1a86407217014572c5
AstroX10 <danielfx0012@gmail.com>
2024-11-21 17:38:48 +0100

    fix: health checks

--- index.js
+++ index.js
@@ -9,44 +9,78 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const log = (type, msg) => console.log(`[${type}] ${msg}`);
 
 async function loadFiles(dir) {
-	try {
-		const files = await readdir(dir, { withFileTypes: true });
-		for (const file of files) {
-			const fullPath = join(dir, file.name);
-			if (file.isDirectory()) await loadFiles(fullPath);
-			else if (extname(file.name) === '.js') await import(`file://${fullPath}`).catch(err => log('ERROR', `File: ${file.name} | ${err.message}`));
-		}
-	} catch (err) {
-		log('ERROR', `Dir ${dir}: ${err.message}`);
-	}
+   try {
+   	const files = await readdir(dir, { withFileTypes: true });
+   	for (const file of files) {
+   		const fullPath = join(dir, file.name);
+   		if (file.isDirectory()) await loadFiles(fullPath);
+   		else if (extname(file.name) === '.js') await import(`file://${fullPath}`).catch(err => log('ERROR', `File: ${file.name} | ${err.message}`));
+   	}
+   } catch (err) {
+   	log('ERROR', `Dir ${dir}: ${err.message}`);
+   }
 }
 
 async function startBot() {
-	try {
-		console.log('XSTRO MD');
-		await config.DATABASE.sync();
-		await loadFiles(join(__dirname, 'lib/sql'));
-		await loadFiles(join(__dirname, 'plugins'));
-		await connect();
-		await config.DATABASE.sync();
-	} catch (err) {
-		log('ERROR', `Boot: ${err.message}`);
-	}
+   try {
+   	console.log('XSTRO MD');
+   	await config.DATABASE.sync();
+   	await loadFiles(join(__dirname, 'lib/sql'));
+   	await loadFiles(join(__dirname, 'plugins'));
+   	await connect();
+   	await config.DATABASE.sync();
+   } catch (err) {
+   	log('ERROR', `Boot: ${err.message}`);
+   }
 }
+
 (async () => {
-	const server = net.createServer(socket => {
-		socket.write('HTTP/1.1 200 OK\r\n');
-		socket.write('Content-Length: 0\r\n');
-		socket.write('\r\n');
-		socket.end();
-	});
-	server.listen(8000, () => {});
-	server.on('error', err => {});
+   const server = net.createServer((socket) => {
+       const responseHeaders = 'HTTP/1.1 200 OK\r\n' +
+                               'Content-Type: text/plain\r\n' +
+                               'Connection: keep-alive\r\n' +
+                               'Keep-Alive: timeout=5, max=1000\r\n' +
+                               'Content-Length: 2\r\n\r\n' +
+                               'OK';
+
+       const keepAliveInterval = setInterval(() => {
+           try {
+               socket.write(responseHeaders);
+           } catch (error) {
+               log('SOCKET ERROR', `Failed to write to socket: ${error.message}`);
+               clearInterval(keepAliveInterval);
+               socket.destroy();
+           }
+       }, 1000);
+
+       socket.on('error', (err) => {
+           log('SOCKET ERROR', `Socket error: ${err.message}`);
+           clearInterval(keepAliveInterval);
+           socket.destroy();
+       });
+
+       socket.on('close', () => {
+           log('SOCKET', 'Socket closed');
+           clearInterval(keepAliveInterval);
+       });
+   });
+
+   server.on('error', (err) => {
+       log('SERVER ERROR', `Server error: ${err.message}`);
+       if (err.code === 'EADDRINUSE') {
+           log('SERVER', 'Port 8000 is already in use');
+       }
+       process.exit(1);
+   });
+
+   server.listen(8000, () => {
+       log('SERVER', 'TCP Health Check Server running on port 8000');
+   });
 })();
 
 startBot();
 
 process.on('SIGINT', () => {
-	config.DATABASE.close();
-	process.exit();
+   config.DATABASE.close();
+   process.exit();
 });
