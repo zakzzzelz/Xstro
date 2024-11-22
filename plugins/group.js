ca8e84d692d9806e6ce327e47c432c5c22f00fa2
AstroX10 <danielfx0012@gmail.com>
2024-11-22 07:48:31 +0100

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -186,6 +186,7 @@ bot(
 		if (!message.isGroup) return message.sendReply('_For groups only!_');
 		if (!m.isAdmin && !m.isBotAdmin) return message.sendReply('_For Admins Only!_');
 		const subject = match || message.quoted?.text;
+		if (!subject) return message.sendReply('_Provide A New Name for the Group!_');
 		await client.groupUpdateSubject(message.jid, subject);
 		return message.sendReply('_Group Name Updated_');
 	},
