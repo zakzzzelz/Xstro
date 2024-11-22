6fe4a66fe7f9b427ca6bfbee5da3484604f22d05
AstroX10 <danielfx0012@gmail.com>
2024-11-22 19:13:35 +0100

    fix: updater

--- plugins/update.js
+++ plugins/update.js
@@ -1,6 +1,7 @@
 import { bot } from '../lib/handler.js';
 import { exec } from 'child_process';
 import simplegit from 'simple-git';
+import { restartProcess } from '../lib/utils.js';
 
 const git = simplegit();
 bot(
@@ -13,16 +14,14 @@ bot(
 	async (message, match) => {
 		await git.fetch();
 		const commits = await git.log(['master..origin/master']);
-		if (commits.total === 0) return await message.sendReply('```No changes in the latest commit```');
+		if (commits.total === 0) return await message.sendReply('```YOU ARE ON THE LATEST VERSION```');
 		if (match === 'now') {
-			await message.sendReply('*Updating...*');
+			await message.sendReply('_Updating && Upgrading_');
 			exec('git stash && git pull origin master', async (err, stderr) => {
 				if (err) return await message.sendReply('```' + stderr + '```');
-				await message.sendReply('*Restarting...*');
-				exec(require('../package.json').scripts.start, (err, _, stderr) => {
-					if (err) return message.sendReply('```' + stderr + '```');
-					message.sendReply('*Restart complete*');
-				});
+				await message.sendReply('_Rebooting Bot_');
+				restartProcess();
+				message.sendReply('_Restart Complete_');
 			});
 		} else {
 			let changes = '_New update available!_\n\n' + '*Patches:* ```' + commits.total + '```\n' + '*Changes:*\n' + commits.all.map((c, i) => '```' + (i + 1) + '. ' + c.message + '```').join('\n') + '\n*To update, send* ```' + message.prefix + 'update now```';
