37e12bdada9e1a64bed2f1a078ffe98c4b9d22d4
AstroX10 <danielfx0012@gmail.com>
2024-11-25 20:26:08 +0100

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -1,68 +1,154 @@
-import { bot } from '../lib/handler.js';
-import { InstaDL, Tiktok, twitter, YTV } from './client/scrapers.js';
-import { extractUrlFromMessage } from '../lib/utils.js';
+import config from '../config.js'
+import { bot } from '../lib/handler.js'
+import { extractUrlFromMessage, getBuffer, getJson } from '../lib/utils.js'
+
+const base_url = 'https://api.giftedtech.my.id/api/download/'
+const { API_KEY } = config
+
+bot(
+	{
+		pattern: 'facebook',
+		isPublic: true,
+		desc: 'Downloads Facebook Videos',
+		type: 'download'
+	},
+	async (message, match) => {
+		const req = extractUrlFromMessage(match || message.quoted?.text)
+		if (!req) return message.sendReply('```I need Facebook Url!````')
+		const res = await getJson(`${base_url}facebook?apikey=${API_KEY}&url=${req}`)
+		const buff = await getBuffer(res.result.hd_video)
+		return await message.send(buff)
+	}
+)
+
+bot(
+	{
+		pattern: 'insta',
+		isPublic: true,
+		desc: 'Downloads Instagram Videos',
+		type: 'download'
+	},
+	async (message) => {
+		const req = extractUrlFromMessage(match || message.quoted?.text)
+		if (!req) return message.sendReply('```I need Instagram Url!````')
+		const res = await getJson(`${base_url}instadl?apikey=${API_KEY}&url=${req}`)
+		const buff = await getBuffer(res.result.download_url)
+		return await message.send(buff)
+	}
+)
 
 bot(
 	{
 		pattern: 'twitter',
 		isPublic: true,
-		desc: 'downloads x videos',
-		type: 'download',
+		desc: 'Downloads Videos from twiiter',
+		type: 'download'
 	},
 	async (message, match) => {
-		const id = match || message.quoted?.text;
-		const url = extractUrlFromMessage(id);
-		if (!url) return message.sendReply('_Provide X Url_');
-		const media = await twitter(url);
-		const { buffer, caption } = media;
-		return await message.send(buffer, { caption: caption });
-	},
-);
+		const req = extractUrlFromMessage(match || message.quoted?.text)
+		if (!req) return message.sendReply('```I need Twitter Url!````')
+		const res = await getJson(`${base_url}twitter?apikey=${API_KEY}&url=${req}`)
+		const buff = await getBuffer(res.result.downloads[0].url)
+		return await message.send(buff)
+	}
+)
 
 bot(
 	{
 		pattern: 'tiktok',
 		isPublic: true,
-		desc: 'downloads tiktok videos',
-		type: 'download',
+		desc: 'Downloads Tiktok Videos',
+		type: 'download'
 	},
 	async (message, match) => {
-		const tiktokUrl = match || message.quoted?.text;
-		const url = extractUrlFromMessage(tiktokUrl);
-		if (!url || !url.includes('vm.tiktok.com')) return message.sendReply('_Provide Tiktok Url!_');
-		const data = await Tiktok(url);
-		return await message.send(data.buffer, { caption: data.desc });
+		const req = extractUrlFromMessage(match || message.quoted?.text)
+		if (!req) return message.sendReply('```I need Tiktok Url!````')
+		const res = await getJson(`${base_url}tiktokdlv1?apikey=${API_KEY}&url=${req}`)
+		const buff = await getBuffer(res.result.video.noWatermark)
+		return await message.send(buff)
+	}
+)
+
+bot(
+	{
+		pattern: 'tikslide',
+		isPublic: true,
+		desc: 'Downloads Tiktok Slides',
+		type: 'download'
 	},
+	async (message, match) => {
+		const req = extractUrlFromMessage(match || message.quoted?.text);
+		if (!req) return message.sendReply('```I need a TikTok URL!```');
+		const res = await getJson(`${base_url}tiktokslide?apikey=${API_KEY}&url=${req}`);
+		for (const slideUrl of res.results) {
+			const buff = await getBuffer(slideUrl);
+			await message.send(buff);
+		}
+	}
 );
 
 bot(
 	{
-		pattern: 'instagram',
-		isPublic: true,
-		desc: 'downloads instagram videos',
-		type: 'download',
+		pattern: 'tgs',
+		isPublic: false,
+		desc: 'Downloads telegram Stickers',
+		type: 'download'
 	},
 	async (message, match) => {
-		const insta = match || message.quoted?.text;
-		const url = extractUrlFromMessage(insta);
-		if (!url) return message.sendReply('_Need Insta Link!_');
-		const data = await InstaDL(url);
-		return await message.send(data);
+		const req = extractUrlFromMessage(match || message.quoted?.text);
+		if (!req) return message.sendReply('```I need a Telegram Stickers Pack URL!```');
+		const res = await getJson(`${base_url}tgs?apikey=${API_KEY}&url=${req}`);
+		for (const sticker of res.results) {
+			const buff = await getBuffer(sticker)
+			await message.send(buff)
+		}
+	}
+)
+
+bot(
+	{
+		pattern: 'mediafire',
+		isPublic: false,
+		desc: 'Downloads Mediafire files',
+		type: 'download'
 	},
-);
+	async (message, match) => {
+		const req = extractUrlFromMessage(match || message.quoted?.text);
+		if (!req) return message.sendReply('```I need a Mediafire URL!```');
+		const res = await getJson(`${base_url}mediafiredl?apikey=${API_KEY}&url=${req}`);
+		const buff = await getBuffer(res.result.downloadLink)
+		return await message.send(buff)
+	}
+)
 
 bot(
 	{
 		pattern: 'ytv',
-		isPublic: true,
-		desc: 'downloads youtube videos',
-		type: 'download',
+		isPublic: false,
+		desc: 'Downloads Youtube Videos',
+		type: 'download'
 	},
 	async (message, match) => {
-		const yturl = match || message.quoted?.text;
-		const url = extractUrlFromMessage(yturl);
-		if (!url) return message.sendReply('_I need youtube url_');
-		const video = await YTV(url);
-		return await message.send(video.buffer, { caption: video.title });
+		const req = extractUrlFromMessage(match || message.quoted?.text);
+		if (!req) return message.sendReply('```I need a YOUTUBE URL!```');
+		const res = await getJson(`${base_url}ytvideo?apikey=${API_KEY}&url=${req}`);
+		const buff = await getBuffer(res.result.download_url)
+		return await message.send(buff, { caption: res.result.title })
+	}
+)
+
+bot(
+	{
+		pattern: 'yta',
+		isPublic: false,
+		desc: 'Downloads Youtube Audio',
+		type: 'download'
 	},
-);
+	async (message, match) => {
+		const req = extractUrlFromMessage(match || message.quoted?.text);
+		if (!req) return message.sendReply('```I need a YOUTUBE URL!```');
+		const res = await getJson(`${base_url}ytvideo?apikey=${API_KEY}&url=${req}`);
+		const buff = await getBuffer(res.result.download_url)
+		return await message.send(buff)
+	}
+)
