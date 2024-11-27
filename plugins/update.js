600b6c4c71232cbd3b5f12c00c07da50c8f176e8
AstroX10 <danielfx0012@gmail.com>
2024-11-27 09:15:18 +0100

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,8 +1,10 @@
 import { bot } from '../lib/handler.js';
+import { fancy } from './client/font.js'
 import { exec } from 'child_process';
 import simplegit from 'simple-git';
 import { manageProcess } from '../lib/utils.js';
 import { updateHerokuApp } from './client/heroku.js';
+import config from '../config.js';
 
 const git = simplegit();
 bot(
@@ -15,18 +17,22 @@ bot(
 	async (message, match) => {
 		await git.fetch();
 		const commits = await git.log(['master..origin/master']);
-		if (commits.total === 0) return await message.sendReply('```YOU ARE ON THE LATEST VERSION```');
+		if (commits.total === 0) return await message.sendReply(fancy(`you are on version ${config.VERSION}`));
 		if (match === 'now') {
-			await message.sendReply('_Updating && Upgrading_');
+			message.sendReply(fancy('restarting bot'))
 			exec('git stash && git pull origin master', async (err, stderr) => {
 				if (err) return await message.sendReply('```' + stderr + '```');
-				await message.sendReply('_Rebooting Bot_');
 				await manageProcess('restart');
-				message.sendReply('_Restart Complete_');
 			});
 		} else {
-			let changes = '_New update available!_\n\n' + '*Patches:* ```' + commits.total + '```\n' + '*Changes:*\n' + commits.all.map((c, i) => '```' + (i + 1) + '. ' + c.message + '```').join('\n') + '\n*To update, send* ```' + message.prefix + 'update now```';
+			let patches = `*Patches:* \`\`\`${commits.total}\`\`\``;
+			let changesList = commits.all
+				.map((c, i) => `\`\`\`${i + 1}. ${c.message}\`\`\``)
+				.join('\n');
+
+			let changes = `*New Update*\n\n${patches}\n*Changes:*\n${changesList}\n\n*Use:* \`\`\`${message.prefix}update now\`\`\``;
 			await message.sendReply(changes);
+
 		}
 	},
 );
@@ -39,7 +45,7 @@ bot(
 		type: 'system'
 	},
 	async (message) => {
-		if (!process.env.HEROKU_APP_NAME && !process.env.HEROKU_API_KEY) return message.sendReply('```HEROKU API KEY OR APP NAME NOT FOUND | INVAILD REQUEST```')
+		if (!process.env.HEROKU_APP_NAME && !process.env.HEROKU_API_KEY) return message.sendReply(fancy('invaild heroku app, make sure you are running on heroku with the correct variables'))
 		await updateHerokuApp()
 	}
 )
