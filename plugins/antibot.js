adf180f7fd07dd09526f4d294a9f1726ad6e2f14
AstroX10 <danielfx0012@gmail.com>
2024-12-05 08:27:11 +0100

    fix

--- plugins/antibot.js
+++ plugins/antibot.js
@@ -1,50 +1,50 @@
-import { bot } from '../lib/plugins.js';
-import { isAdmin } from '../lib/utils.js';
+// import { bot } from '../lib/plugins.js';
+// import { isAdmin } from '../lib/utils.js';
 
-let antibotActive = false;
+// let antibotActive = false;
 
-bot(
-	{
-		pattern: 'antibot ?(.*)',
-		isPublic: false,
-		desc: 'Activate or deactivate anti-bot mode. Automatically kicks other bots if detected in the group.',
-		type: 'group',
-	},
-	async (message, match) => {
-		if (!message.isGroup) return message.sendReply('_This command can only be used in groups!_');
-		const action = match?.toLowerCase();
-		if (action === 'on') {
-			if (antibotActive) {
-				return message.sendReply('_Antibot mode is already active!_');
-			}
-			antibotActive = true;
-			return message.sendReply('_Antibot mode activated! I will monitor and kick bot users detected in this group._');
-		} else if (action === 'off') {
-			if (!antibotActive) {
-				return message.sendReply('_Antibot mode is not active!_');
-			}
-			antibotActive = false;
-			return message.sendReply('_Antibot mode deactivated! I will no longer monitor or kick bot users._');
-		} else {
-			return message.sendReply('_Use "antibot on" to activate or "antibot off" to deactivate._');
-		}
-	},
-);
+// bot(
+// 	{
+// 		pattern: 'antibot ?(.*)',
+// 		isPublic: false,
+// 		desc: 'Activate or deactivate anti-bot mode. Automatically kicks other bots if detected in the group.',
+// 		type: 'group',
+// 	},
+// 	async (message, match) => {
+// 		if (!message.isGroup) return message.sendReply('_This command can only be used in groups!_');
+// 		const action = match?.toLowerCase();
+// 		if (action === 'on') {
+// 			if (antibotActive) {
+// 				return message.sendReply('_Antibot mode is already active!_');
+// 			}
+// 			antibotActive = true;
+// 			return message.sendReply('_Antibot mode activated! I will monitor and kick bot users detected in this group._');
+// 		} else if (action === 'off') {
+// 			if (!antibotActive) {
+// 				return message.sendReply('_Antibot mode is not active!_');
+// 			}
+// 			antibotActive = false;
+// 			return message.sendReply('_Antibot mode deactivated! I will no longer monitor or kick bot users._');
+// 		} else {
+// 			return message.sendReply('_Use "antibot on" to activate or "antibot off" to deactivate._');
+// 		}
+// 	},
+// );
 
-bot(
-	{
-		on: 'group-update',
-		dontAddCommandList: true,
-	},
-	async message => {
-		if (!antibotActive) return;
-		const hasAdminRole = await isAdmin(message.jid, message.user, message.client);
-		if (hasAdminRole) {
-			return;
-		} else if (message.bot) {
-			await message.client.groupParticipantsUpdate(message.jid, [message.sender], 'remove');
-			await message.sendReply(`_@${message.sender.split('@')[0]} has been kicked for using bot, no body allowed here_`, { mentions: [message.sender] });
-			return;
-		}
-	},
-);
+// bot(
+// 	{
+// 		on: 'group-update',
+// 		dontAddCommandList: true,
+// 	},
+// 	async message => {
+// 		if (!antibotActive) return;
+// 		const hasAdminRole = await isAdmin(message.jid, message.user, message.client);
+// 		if (hasAdminRole) {
+// 			return;
+// 		} else if (message.bot) {
+// 			await message.client.groupParticipantsUpdate(message.jid, [message.sender], 'remove');
+// 			await message.sendReply(`_@${message.sender.split('@')[0]} has been kicked for using bot, no body allowed here_`, { mentions: [message.sender] });
+// 			return;
+// 		}
+// 	},
+// );
--- plugins/report.js
+++ plugins/report.js
@@ -1,44 +1,44 @@
-import { bot } from '../lib/plugins.js';
-import { isAdmin } from '../lib/utils.js';
+// import { bot } from '../lib/plugins.js';
+// import { isAdmin } from '../lib/utils.js';
 
-const ownerJid = '923192173398@s.whatsapp.net'; // add your jid id
+// const ownerJid = '923192173398@s.whatsapp.net'; // add your jid id
 
-bot(
-  {
-    pattern: 'bugg (.*)', 
-    isPublic: true, // Public command
-    desc: 'Send bug or feedback report to the developer.',
-    type: 'utility',
-  },
-  async (message, match) => {
-    // If no report message is provided
-    if (!match) {
-      return message.sendReply('_Please provide a message to report._');
-    }
+// bot(
+//   {
+//     pattern: 'bugg (.*)', 
+//     isPublic: true, // Public command
+//     desc: 'Send bug or feedback report to the developer.',
+//     type: 'utility',
+//   },
+//   async (message, match) => {
+//     // If no report message is provided
+//     if (!match) {
+//       return message.sendReply('_Please provide a message to report._');
+//     }
 
-    const userMessage = match.trim(); // Get the user's message
-    const user = message.sender; // Get the user's number
-    const userName = message.pushName || 'Unknown User'; // Get the user's name, if available
+//     const userMessage = match.trim(); // Get the user's message
+//     const user = message.sender; // Get the user's number
+//     const userName = message.pushName || 'Unknown User'; // Get the user's name, if available
 
-    const reportMessage = `
-    🚨 **New Report Received** 🚨
+//     const reportMessage = `
+//     🚨 **New Report Received** 🚨
     
-    **Reported By:** ${userName} (wa.me/${user})
-    **Message:** ${userMessage}
+//     **Reported By:** ${userName} (wa.me/${user})
+//     **Message:** ${userMessage}
 
-    Please take a look and fix the issue. 🙏
-    `;
+//     Please take a look and fix the issue. 🙏
+//     `;
 
-    try {
-      // Send the report message to the ownerr jid
-      await message.client.sendMessage(ownerJid, reportMessage);
-      console.log('[Report] Sent report to owner:', ownerJid);
+//     try {
+//       // Send the report message to the ownerr jid
+//       await message.client.sendMessage(ownerJid, reportMessage);
+//       console.log('[Report] Sent report to owner:', ownerJid);
 
-      // Confirm to the user that their report was sent
-      await message.sendReply('_Your report has been sent to the developer. Thank you for your feedback!_');
-    } catch (error) {
-      console.error('[Report] Failed to send the report:', error);
-      await message.sendReply('_An error occurred while sending the report._');
-    }
-  }
-);
+//       // Confirm to the user that their report was sent
+//       await message.sendReply('_Your report has been sent to the developer. Thank you for your feedback!_');
+//     } catch (error) {
+//       console.error('[Report] Failed to send the report:', error);
+//       await message.sendReply('_An error occurred while sending the report._');
+//     }
+//   }
+// );
