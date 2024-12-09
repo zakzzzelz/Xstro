44449ea436b15fb97ab0289d421be8e79f7df4d9
AstroX10 <danielfx0012@gmail.com>
2024-12-09 08:46:45 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -14,37 +14,31 @@ bot(
 	},
 	async message => {
 		let menuText = `\`\`\`
-╭── ${config.BOT_INFO.split(';')[1]} ───
+╭────────────────
+│ ${config.BOT_INFO.split(';')[1]}
+│ \t\t ${config.VERSION}
 │ User: ${message.pushName}
 │ Mode: ${config.MODE}
 │ Uptime: ${runtime(process.uptime())}
 │ Platform: ${platform()}
 │ Plugins: ${commands.length}
 │ Memory: ${formatBytes(totalmem() - freemem())}
-│ Version: ${config.VERSION}
 ╰────────────────
 \`\`\`\n`;
 
 		let commandCounter = 1;
-		const categorized = commands
+		const allCommands = commands
 			.filter(cmd => cmd.pattern && !cmd.dontAddCommandList)
-			.map(cmd => ({
-				name: cmd.pattern.toString().split(/\W+/)[2],
-				category: cmd.type?.toLowerCase() || 'misc',
-			}))
-			.reduce((acc, { name, category }) => {
-				acc[category] = (acc[category] || []).concat(name);
-				return acc;
-			}, {});
+			.map(cmd => cmd.pattern.toString().split(/\W+/)[2])
+			.sort(); // Sort commands alphabetically
 
-		Object.keys(categorized).forEach(category => {
-			menuText += `\n╭── *${category.toUpperCase()}* ────\n`;
-			categorized[category].forEach(cmd => {
-				menuText += fancy(`│▸ ${commandCounter}. ${cmd}\n`);
-				commandCounter++;
-			});
-			menuText += `╰──────────────\n`;
+		menuText += `\t\t ${fancy(`COMMANDS LIST`)} \n\n\n╭────────────\n`;
+		allCommands.forEach(cmd => {
+			menuText += fancy(`│▸ ${commandCounter}. ${cmd}\n`);
+			commandCounter++;
 		});
+		menuText += `╰──────────────\n`;
+
 		const gif = readFileSync('./media/intro.mp4');
 		return message.send(gif, { caption: menuText, gifPlayback: true });
 	},
