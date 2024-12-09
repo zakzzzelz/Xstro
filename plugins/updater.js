de9ffec7ac0c56c86bc9816317499b52513c84af
AstroX10 <danielfx0012@gmail.com>
2024-12-09 17:55:02 +0100

    fix

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,44 +1,21 @@
 import { bot } from '../lib/plugins.js';
-import { exec } from 'child_process';
-import { manageProcess } from '../lib/utils.js';
-import { performUpdate, updateHerokuApp } from '../lib/updater.js';
+import { isLatest, updateBot } from '../lib/updater.js';
 
 bot(
 	{
 		pattern: 'update',
 		isPublic: false,
-		desc: 'Update the bot',
-		type: 'system',
+		desc: 'Updates Bot',
 	},
 	async (message, match) => {
-		const updateInfo = await performUpdate();
-		if (typeof updateInfo === 'string') {
-			message.send('```' + updateInfo + '```');
+		const updated = await isLatest();
+		if (updated) return message.send('```You are on the Latest Update```');
+		if (!match === 'now') return message.send('```Invaild, use ' + message.prefix + 'update now```');
+		const updater = await updateBot();
+		if (updater) {
+			message.send('```Update Success```');
 		} else {
-			if (match === 'now') {
-				message.send('Restarting bot...');
-				exec('git stash && git pull origin master', (pullErr, stderr) => {
-					if (pullErr) return message.send('Error pulling updates: ' + stderr);
-					manageProcess('restart');
-				});
-			} else {
-				const changes = `*New Update*\n\n*Changes:*\n${updateInfo}\n\n*Use:* ${message.prefix}update now`;
-				message.send('```' + changes + '```');
-			}
+			message.send('```Failed to update bot```');
 		}
-	},
-);
-
-bot(
-	{
-		pattern: 'redeploy',
-		isPublic: false,
-		desc: 'Fully Updates & Redeploy Heroku App',
-		type: 'system',
-	},
-	async message => {
-		if (!process.env.HEROKU_APP_NAME && !process.env.HEROKU_API_KEY) return message.send('invaild heroku app, make sure you are running on heroku with the correct variables');
-		message.send('Redeploying Heroku Dyno\n5mins');
-		await updateHerokuApp();
 	},
 );
