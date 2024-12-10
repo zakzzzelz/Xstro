76a901b5a3aa948d6e419f3f30ab4733b5718c74
AstroX10 <danielfx0012@gmail.com>
2024-12-10 10:48:03 +0100

    fix

---
+++ plugins/plugin.js
@@ -0,0 +1,54 @@
+import { bot } from '../lib/plugins.js';
+import { extractUrlFromString } from 'utils';
+import { installPlugin, removePluginByName, listPlugins } from '../lib/pluginManager.js';
+
+bot(
+	{
+		pattern: 'install ?(.*)',
+		isPublic: false,
+		desc: 'Installs a Plugin',
+	},
+	async (message, match) => {
+		const pluginUrl = extractUrlFromString(match || message.reply_message?.text);
+		if (!pluginUrl.startsWith('https://gist.githubusercontent.com')) return message.send('_Provide a valid Plugin URL_');
+
+		try {
+			const pluginName = await installPlugin(pluginUrl);
+			message.send(`_${pluginName} plugin installed_`);
+		} catch (error) {
+			message.send(`_Error: ${error.message}_`);
+		}
+	},
+);
+
+bot(
+	{
+		pattern: 'delplugin ?(.*)',
+		isPublic: false,
+		desc: 'Deletes a Plugin',
+	},
+	async (message, match) => {
+		if (!match) return message.send('_Provide an installed plugin name_');
+		const pluginName = `${match.trim()}.js`;
+
+		const removed = await removePluginByName(pluginName);
+		if (removed) {
+			message.send(`_${pluginName} plugin uninstalled_`);
+		} else {
+			message.send('_Plugin not found_');
+		}
+	},
+);
+
+bot(
+	{
+		pattern: 'getplugins',
+		isPublic: false,
+		desc: 'Lists all installed plugins',
+	},
+	async message => {
+		const plugins = await listPlugins();
+		const pluginList = plugins.length > 0 ? `_Plugins Installed:_\n${plugins.join('\n')}` : '_No plugins installed_';
+		message.send(pluginList);
+	},
+);
--- plugins/system.js
+++ plugins/system.js
@@ -1,26 +1,16 @@
-import fs from 'fs';
-import path, { join, basename, extname } from 'path';
-import axios from 'axios';
 import { exec } from 'child_process';
 import { performance } from 'perf_hooks';
 import { bot } from '../lib/plugins.js';
 import { manageProcess, runtime } from '../lib/utils.js';
-import { addPlugin, getPlugins, removePlugin } from '../sql/plugins.js';
 import { manageVar } from './bot/tools.js';
 import { fancy } from './bot/font.js';
-import { extractUrlFromString, getBuffer, getJson } from 'utils';
-
-const envFilePath = path.join(process.cwd(), '.env');
-const envfile = () => {
-	if (!fs.existsSync(envFilePath)) fs.writeFileSync(envFilePath, '', 'utf-8');
-};
+import { getBuffer, getJson } from 'utils';
 
 bot(
 	{
 		pattern: 'ping',
 		isPublic: true,
 		desc: 'Get Performance',
-		type: 'system',
 	},
 	async message => {
 		const start = performance.now();
@@ -35,7 +25,6 @@ bot(
 		pattern: 'runtime',
 		isPublic: true,
 		desc: 'Get Runtime of bot',
-		type: 'system',
 	},
 	async message => {
 		return await message.send(fancy(`*Uptime: ${runtime(process.uptime())}*`));
@@ -44,68 +33,9 @@ bot(
 
 bot(
 	{
-		pattern: 'install ?(.*)',
-		isPublic: false,
-		desc: 'Installs a Plugin',
-		type: 'system',
-	},
-	async (message, match) => {
-		const pluginUrl = extractUrlFromString(match.trim() || message.reply_message?.text);
-		if (!pluginUrl.startsWith('https://gist.githubusercontent.com')) return message.send('_Provide a valid Plugin URL_');
-
-		const pluginName = `${basename(pluginUrl, extname(pluginUrl))}.js`;
-		const existingPlugins = await getPlugins();
-		if (existingPlugins.some(plugin => plugin.name === pluginName)) return message.send('_Plugin already installed_');
-
-		const pluginPath = join('plugins', pluginName);
-		const response = await axios.get(pluginUrl, { responseType: 'arraybuffer' });
-		fs.writeFileSync(pluginPath, response.data);
-		await addPlugin(pluginName);
-		message.send(`_${pluginName} plugin installed_`);
-	},
-);
-
-bot(
-	{
-		pattern: 'delplugin ?(.*)',
-		isPublic: false,
-		desc: 'Deletes a Plugin',
-		type: 'system',
-	},
-	async (message, match) => {
-		if (!match) return message.send('_Provide an installed plugin name_');
-		const baseName = match.trim();
-		const pluginName = `${baseName}.js`;
-
-		const deleted = await removePlugin(pluginName);
-		if (!deleted) return message.send('_Plugin not found_');
-
-		const pluginPath = join('plugins', pluginName);
-		if (fs.existsSync(pluginPath)) fs.unlinkSync(pluginPath);
-		message.send(`_${pluginName} plugin uninstalled_`);
-	},
-);
-
-bot(
-	{
-		pattern: 'getplugins',
-		isPublic: false,
-		desc: 'Lists all installed plugins',
-		type: 'system',
-	},
-	async message => {
-		const plugins = await getPlugins();
-		const pluginList = plugins.length > 0 ? `_Plugins Installed:_\n${plugins.map(plugin => plugin.name).join('\n')}` : '_No plugins installed_';
-		message.send(pluginList);
-	},
-);
-
-bot(
-	{
 		pattern: 'setvar',
 		isPublic: false,
 		desc: 'Set system var',
-		type: 'system',
 	},
 	async (message, match) => {
 		envfile();
@@ -123,7 +53,6 @@ bot(
 		pattern: 'delvar',
 		isPublic: false,
 		desc: 'Delete system var',
-		type: 'system',
 	},
 	async (message, match) => {
 		envfile();
@@ -139,7 +68,6 @@ bot(
 		pattern: 'getvar',
 		isPublic: false,
 		desc: 'Get system vars',
-		type: 'system',
 	},
 	async message => {
 		envfile();
@@ -153,7 +81,6 @@ bot(
 		pattern: 'restart',
 		isPublic: false,
 		desc: 'Restarts Bot',
-		type: 'system',
 	},
 	async message => {
 		await message.send('_Restarting application..._');
@@ -166,7 +93,6 @@ bot(
 		pattern: 'shutdown',
 		isPublic: false,
 		desc: 'Off Bot',
-		type: 'system',
 	},
 	async message => {
 		await message.send('_Shutting Down application..._');
@@ -174,13 +100,11 @@ bot(
 	},
 );
 
-
 bot(
 	{
 		pattern: 'shell ?(.*)',
 		isPublic: false,
 		desc: 'Run shell commands',
-		type: 'system',
 	},
 	async (message, match) => {
 		if (!match) return message.send('_Provide a shell command to run_');
@@ -198,7 +122,6 @@ bot(
 		pattern: 'logout',
 		isPublic: false,
 		desc: 'End your Xstro Session',
-		type: 'system',
 	},
 	async (message, match) => {
 		if (!match) return message.send(`*Hello ${message.pushName} this isn't the goo, goo ga ga, this command will logout you out of your Xstro Session, and you will be unable to use this bot until you get a new session*\nAre you sure you want to continue with this decision, then type\n${message.prefix}logout confirm`);
@@ -216,7 +139,6 @@ bot(
 		pattern: 'fetch',
 		isPublic: true,
 		desc: 'Get data from internet',
-		type: 'system',
 	},
 	async (message, match) => {
 		if (!match) return message.send('_I need a URL_');
@@ -243,4 +165,13 @@ bot(
 			await message.send('_Invalid mode. Use "json" or "buffer"._');
 		}
 	},
+);
+
+bot(
+	{
+		pattern: 'cpu',
+		isPublic: false,
+		desc: 'Get Cpu Information',
+	},
+	async message => {},
 );
