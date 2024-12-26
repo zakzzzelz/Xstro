f704bb92ecb5711c43f436c3fcc0c829b1a9b6a1
AstroX10 <danielfx0012@gmail.com>
2024-12-26 12:42:32 +0100

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,5 +1,6 @@
 import { bot } from '#lib';
 import { XSTRO } from '#utils';
+import { getJson } from 'xstro-utils';
 
 bot(
 	{
@@ -11,6 +12,25 @@ bot(
 	async (message, match) => {
 		const msg = await message.send('*Thinking*');
 		const res = await XSTRO.chatbot(match || message.reply_message.text);
+		return await msg.edit(res);
+	},
+);
+
+bot(
+	{
+		pattern: 'llama',
+		public: true,
+		desc: 'Chat with llama',
+		type: 'ai',
+	},
+	async (message, match) => {
+		if (!match) return message.send('_How can i help?_');
+		const msg = await message.send('*hmm*');
+		const res = (
+			await getJson(
+				`https://api.gurusensei.workers.dev/llama?prompt=${match}`,
+			)
+		).response.response;
 		return await msg.edit(res);
 	},
 );
