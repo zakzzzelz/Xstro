a6254c2002836860e1d271641f32d6d382fdc3ba
AstroX10 <danielfx0012@gmail.com>
2024-12-31 07:29:08 +0100

    fix

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,5 +1,5 @@
 import { bot } from '#lib';
-import { XSTRO } from '#utils';
+import { upload, XSTRO } from '#utils';
 import { getBuffer, getJson } from 'xstro-utils';
 
 bot(
@@ -24,7 +24,7 @@ bot(
 		type: 'ai',
 	},
 	async (message, match) => {
-		if (!match) return message.send('_How can i help?_');
+		if (!match && !message.reply_message?.text) return message.send('_How can i help?_');
 		const msg = await message.send('*hmm*');
 		const res = (await getJson(`https://api.gurusensei.workers.dev/llama?prompt=${match}`)).response.response;
 		return await msg.edit(res);
@@ -41,6 +41,53 @@ bot(
 	async (message, match) => {
 		if (!match) return message.send('_Give me a prompt!_');
 		const res = await getBuffer(`https://api.gurusensei.workers.dev/dream?prompt=${match}`);
+		return await message.send(res, {
+			caption: `_Here is your image for:_ ${match}`,
+		});
+	},
+);
+
+bot(
+	{
+		pattern: 'gemini',
+		public: true,
+		desc: 'Chat with Gemini Ai',
+		type: 'ai',
+	},
+	async (message, match) => {
+		if (!match && !message.reply_message?.text) return message.send('_How can i help?_');
+		const msg = await message.send('*Thinkg*');
+		const res = await getJson(`https://bk9.fun/ai/gemini?q=${match || message.reply_message.text}`).BK9;
+		return msg.edit(res);
+	},
+);
+
+bot(
+	{
+		pattern: 'askimg',
+		public: true,
+		desc: 'Ask Ai about an image',
+		type: 'ai',
+	},
+	async (message, match) => {
+		if (!match || !message.reply_message.image) return message.send('_Reply An Image and ask me questions about it_');
+		const image = await message.download();
+		const { rawUrl } = await upload(image);
+		const res = (await getJson(`https://bk9.fun/ai/geminiimg?url=${rawUrl}&q=${match}`)).BK9;
+		return message.send(res);
+	},
+);
+
+bot(
+	{
+		pattern: 'dalle',
+		public: true,
+		desc: 'Generates Images',
+		type: 'ai',
+	},
+	async (message, match) => {
+		if (!match) return message.send('_Give me a prompt!_');
+		const res = await getBuffer(`https://bk9.fun/ai/magicstudio?prompt=${match}`);
 		return await message.send(res, {
 			caption: `_Here is your image for:_ ${match}`,
 		});
