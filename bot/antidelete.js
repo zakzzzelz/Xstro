52e3a5311aebc7e5b1c9deba35814212580cfee6
AstroX11 <danielfx0012@gmail.com>
2025-01-18 20:48:11 +0100

    building

--- bot/antidelete.js
+++ bot/antidelete.js
@@ -4,9 +4,15 @@ import { isJidGroup, jidNormalizedUser } from 'baileys';
 
 export async function AntiDelete(msg) {
   if (!(await getAntiDelete())) return;
-  if (!msg.message?.protocolMessage) return;
+  // console.log(msg.type);
+  // if (msg.type !== 'protocolMessage') return;
+  // console.log(msg.ProtocolType);
+  // if (msg.ProtocolType !== 'REVOKE') return;
 
   const client = msg.client;
+  const store = await msg.client.loadMessage(msg.isProtocol.key.id);
+  // console.log(msg.isProtocol);
+  // console.log(store);
 
   if (isJidGroup(msg.from)) {
     // Antidelete for group chats
