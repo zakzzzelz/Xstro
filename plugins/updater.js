fc93845d971ef06fa569486dcc8c82c4b71b24c7
AstroX11 <danielfx0012@gmail.com>
2025-01-26 19:10:38 +0100

    fix

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,8 +1,8 @@
 import { bot } from '#lib';
 import { exec } from 'child_process';
-import simpleGit from 'simple-git';
+import { promisify } from 'util';
 
-const git = simpleGit();
+const execAsync = promisify(exec);
 
 bot(
   {
@@ -13,54 +13,63 @@ bot(
   },
   async (message, match) => {
     const prefix = message.prefix;
-    await git.fetch();
 
-    const commits = await git.log([`master..origin/master`]);
-    if (!match) {
-      if (commits.total === 0) {
-        return await message.send('No update available');
-      } else {
-        let changes = 'UPDATE FOUND\n\n';
-        changes += `*Changes:* ${commits.total}\n`;
-        changes += '*Updates:*\n';
-        commits.all.forEach((commit, index) => {
-          changes += `${index + 1}. ${commit.message}\n`;
-        });
-        changes += `\n*To update, use* ${prefix}update now`;
-        await message.send(changes);
-      }
-    }
-    if (match && match === 'now') {
-      if (commits.total === 0) {
-        return await message.send('No changes in the latest commit');
-      }
-      await message.send('*Updating...*');
-      exec(`git stash && git pull origin master`, async (err, stdout, stderr) => {
-        if (err) {
-          return await message.send('' + stderr + '');
-        }
-        await message.send('*Restarting...*');
-        const dependency = await updatedDependencies();
-        if (dependency) {
-          await message.send('*Dependencies changed. Installing new dependencies...*');
-          exec(`npm install`, async (err, stdout, stderr) => {
-            if (err) {
-              return await message.send('' + stderr + '');
-            }
-            process.exit(0);
-          });
+    try {
+      await execAsync('git fetch');
+
+      const { stdout: logOutput } = await execAsync('git log master..origin/master');
+      const commits = logOutput
+        .trim()
+        .split('\n')
+        .filter((line) => line.startsWith('commit '));
+
+      if (!match) {
+        if (commits.length === 0) {
+          return await message.send('No update available');
         } else {
-          process.exit(0);
+          let changes = 'Update Available\n\n';
+          changes += `*Changes:* ${commits.length}\n`;
+          changes += '*Updates:*\n';
+
+          const { stdout: messageOutput } = await execAsync(
+            'git log master..origin/master --pretty=format:%s'
+          );
+          const commitMessages = messageOutput.trim().split('\n');
+
+          commitMessages.forEach((msg, index) => {
+            changes += `${index + 1}. ${msg}\n`;
+          });
+
+          changes += `\n*To update, use* ${prefix}update now`;
+          await message.send(changes);
         }
-      });
+      }
+
+      if (match && match === 'now') {
+        if (commits.length === 0) {
+          return await message.send('No changes in the latest commit');
+        }
+
+        await message.send('*Updating...*');
+        await execAsync('git stash && git pull origin master');
+        await message.send('*Restarting...*');
+        const dependencyChanged = await updatedDependencies();
+        if (dependencyChanged) {
+          await message.send('*Dependencies changed. Installing new dependencies...*');
+          await execAsync('npm install');
+        }
+        process.exit(0);
+      }
+    } catch (error) {
+      await message.send(`Error: ${error.message}`);
     }
   }
 );
 
 const updatedDependencies = async () => {
   try {
-    const diff = await git.diff([`master..origin/master`]);
-    return diff.includes('"dependencies":');
+    const { stdout } = await execAsync('git diff master..origin/master -- package.json');
+    return stdout.includes('"dependencies":');
   } catch (error) {
     console.error('Error occurred while checking package.json:', error);
     return false;
